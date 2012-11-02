a426a9b97380bd2360104d7d2d1fc7eb894f3625
Grégoire Beaudoire <gregoire.beaudoire@ens-lyon.fr>
2012-11-02 20:16:55 +0100

    Probablement buggé, j'ai pas encore testé, trop faim pour ça. Je vais manger, je reregarderai dans la soirée voir ce qui ne marche pas (et connaissant mon talent légendaire en programmation et en C, il y en aura forcément).

--- sort.c
+++ sort.c
@@ -12,14 +12,106 @@ bool strCmp(char *c1, char *c2)
 	return (c1[i] < c2[i]);
 }
 
-
-
-void qsort(s_XYCity *tab, int size)
+void echange(s_XYCity **tab, int i, int j)
 {
+	s_XYCity stock;
+	stock = (*tab)[i];
+	(*tab)[i] = (*tab)[j];
+	(*tab)[j] = stock ;
+}
+
+int median(s_XYCity city1, s_XYCity city2, s_XYCity city3, s_XYCity *pivot)
+{
+	if(strCmp(city1.name, city2.name))
+	{
+		if(strCmp(city2.name, city3.name))
+		{
+			*pivot = city2;
+			return 1;
+		}
+		else
+		{
+			if(strCmp(city3.name, city1.name))
+			{
+				*pivot = city1;
+				return 0;
+			}
+			else
+			{
+				*pivot = city3;
+				return 2;
+			}
+		}
+	}
+	else
+	{
+		if(strCmp(city1.name, city3.name)
+		{
+			*pivot = city1;
+			return 0;
+		}
+		else
+		{
+			if(strCmp(city2.name, city3.name))
+			{	
+				*pivot = city3;
+				return 2;
+			}
+			else
+			{
+				*pivot = city2;
+				return 1;
+			}
+		}
+	}
+}
+
+
+int qsort(s_XYCity *tab, int begin, int end)
+{
+	int i, size, pos;
+	size = end-begin+1;
+	s_XYCity pivot;
 	if (size < 3)
 	{
+		for (i = begin; i < end; i++)
+		{
+			if (strCmp(tab[i].name,tab[i+1].name))
+				echange(&tab,i,i+1);
+		}
+		return (begin+1);
 	}
-
-
-	int a;
+	else
+	{
+		pos = median(tab[begin], tab[begin+1], tab[begin+2], &pivot);
+		echange(&tab, begin, begin+pos);
+		pos = begin;
+		for (i = begin+1; i < end; i++)
+		{
+			if (strCmp(tab[i].name,pivot.name))
+			{
+				pos++;
+				echange(&tab, pos, i);
+			}
+		}
+		return pos;
+	}
 }
+
+void qsortbis(s_XYCity *tab, int begin, int end)
+{
+	if (begin < end)
+	{
+		int pivot = qsort(tab, begin, end);
+		qsortbis(tab, begin, pivot-1);
+		qsortbis(tab, pivot+1, end);
+	}
+}
+
+void sort(s_XYCity *tab, int size)
+{
+	qsortbis(tab, 0, size-1);
+}
+		
+		
+		
